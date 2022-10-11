b67a2e0d9995c46944e2b64b804331ff0d7ca2a0
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2022-10-11 16:56:34 +0200

    Rename cog

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,16 +1,16 @@
 from discord.ext import commands
 
-class ping(commands.Cog):
+class startup(commands.Cog):
 	def __init__(self, client):
 		self.client = client 
 	
 	@commands.Cog.listener()
 	async def on_ready(self):
-		print('Up and running.')
+		print('Greetings, puny earth-creature.')
 	
 	@commands.command()
 	async def ping(self, ctx):
 		await ctx.send("I see all.")
 
 def setup(client):
-	client.add_cog(ping(client))
+	client.add_cog(startup(client))
