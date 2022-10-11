5b47264e97897121aa71ffeea75aedee513dec9f
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2022-10-11 16:45:34 +0200

    Woopsie and photo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ client = commands.Bot(command_prefix = ">")
 
 if __name__ == "__main__":
 	for f in os.listdir("./cogs"):
-		if f.endswith(".py")
+		if f.endswith(".py"):
 			client.load_extension("cogs." + f[:-3])
 
 	client.run("token")
